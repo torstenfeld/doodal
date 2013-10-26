a91a64cda001cc3303456337803ec31415e32b7d
Torsten Feld <torsten.feld@avira.com>
2013-10-26 17:34:27 +0200

    added login page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
 App = Ember.Application.create();
 
+/****************************
+* ROUTES
+* **************************/
+
 App.Router.map(function() {
-  // put your routes here
+    this.resource('signup');
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+    model: function() {
+        return ['red', 'yellow', 'blue'];
+    }
 });
+
+/****************************
+* CONTROLLERS
+* **************************/
+
+ App.ApplicationController = Ember.Controller.extend({
+    isLoggedIn: false
+});
+
+App.SignupController = Ember.Controller.extend({
+    email: '',
+    password: '',
+    passwordDidChange: function() {
+        this.set('hashedPw', CryptoJS.SHA3(this.get('password'), {outputLength: 128}));
+    }.observes('password'),
+    hashedPw: null,
+    isEmailValid: true,
+    isPwValid: true,
+    actions: {
+        submit: function() {
+            // send auth to server
+            var obj = {
+                email: this.get('email'),
+                pwhash: this.get('hashedPw').toString()
+            }
+            console.log(JSON.stringify(obj));
+        }
+    }
+
+});
+
+/****************************
+* VIEWS
+* **************************/
